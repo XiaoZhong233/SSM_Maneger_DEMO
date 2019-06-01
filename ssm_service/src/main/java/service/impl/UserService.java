2a9d2494fa726f1f00d2927a79f8403d2d5e94a1
zhong <610596547@qq.com>
2019-06-01 12:37:29 +0800

    update spring-security version

--- ssm_service/src/main/java/service/impl/UserService.java
+++ ssm_service/src/main/java/service/impl/UserService.java
@@ -7,7 +7,6 @@ import domain.Permission;
 import domain.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
